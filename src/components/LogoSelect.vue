542fb7bdb338c5b39554ce987487e40a9b8561c2
livesfeel <2865988204@qq.com>
2020-04-13 10:19:50 +0800

    Restructure

---
+++ src/components/LogoSelect.vue
@@ -0,0 +1,136 @@
+<template>
+  <!-- 选择logo组件 -->
+  <div class="main-logo">
+    <img :src="logoData[selectedNow].imgSrc" @click="toggleFlag" />
+    <span class="logoList-arrow" @click="toggleFlag"></span>
+    <!-- tranition是vue的过渡效果 -->
+    <transition name="logofade">
+      <ul v-show="logoListFlag&&mouseLeaveFlag" class="logoList" @mouseleave="mouseLeaveList">
+        <li
+          v-for="(item,index) of logoData"
+          class="logoItem"
+          @mouseover="logoListHover(index)"
+          :class="{selectback:index==logoNow}"
+          @click="logoSelected(index)"
+          :key="'logo'+index"
+        >
+          <img :src="item.imgSrc" />
+        </li>
+      </ul>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      selectedNow: 0,
+      logoNow: -1,
+      logoListFlag: false,
+      mouseLeaveFlag: false,
+      logoData: [
+        {
+          imgSrc: require("../assets/imgs/baidu_logo.png")
+        },
+        {
+          imgSrc: require("../assets/imgs/360_logo.png")
+        },
+        {
+          imgSrc: require("../assets/imgs/sougou_logo.png")
+        }
+      ]
+    };
+  },
+  methods: {
+    toggleFlag() {
+      this.logoListFlag = !this.logoListFlag;
+      this.mouseLeaveFlag = true;
+    },
+    logoListHover(index) {
+      this.logoNow = index;
+    },
+    logoSelected(index) {
+      this.selectedNow = index;
+      this.logoListFlag = false;
+      /*触发父组件的自定义事件，向父组件传参数,selectNow是选择了哪个搜索引擎的索引，
+      父组件得到了之后就可以指定搜索时跳转到哪个搜索引擎*/
+      this.$emit("getindex", this.selectedNow);
+    },
+    mouseLeaveList() {
+      //鼠标离开列表事件
+      this.mouseLeaveFlag = false;
+      this.logoListFlag = false;
+    }
+  }
+};
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  padding: 0;
+  margin: 0;
+}
+.main-logo {
+  width: 600px;
+  height: 140px;
+  position: relative;
+}
+
+.main-logo img {
+  display: block;
+  margin: 0 auto;
+  user-select: none;
+  cursor: pointer;
+}
+
+.logoList-arrow {
+  position: absolute;
+  width: 0;
+  height: 0;
+  border: 8px solid;
+  border-color: #000 transparent transparent transparent;
+  right: 100px;
+  top: 66px;
+  cursor: pointer;
+}
+
+.logoList {
+  position: absolute;
+  top: 100%;
+  width: 200px;
+  left: 50%;
+  margin-left: -100px;
+  z-index: 999999;
+  border: 1px solid #d4d4d4;
+}
+
+.logoList li {
+  width: 100%;
+  height: 80px;
+  background-color: #fefefe;
+  line-height: 80px;
+  padding-top: 1px;
+}
+
+.logoList li img {
+  width: 100%;
+  margin-top: 10px;
+}
+
+.logofade-enter-active,
+.logofade-leave-active {
+  transition: all 0.5s;
+}
+
+.logofade-enter,
+.logofade-leave-active {
+  opacity: 0;
+  transform: translateY(20px);
+}
+.selectback {
+  background-color: #eee !important;
+  cursor: pointer;
+}
+</style>
